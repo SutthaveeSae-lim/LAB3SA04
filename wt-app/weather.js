0fdc8f2ba6b88a7e0f6eed4b66af8a62986abec9
SutthaveeSae-lim <6210110379@psu.ac.th>
2022-08-17 21:24:52 +0700

    part3

--- wt-app/weather.js
+++ wt-app/weather.js
@@ -1,29 +1,27 @@
+import React,{useState} from "react";
+import { StyleSheet,View, Text,ImageBackground } from "react-native";
+import Forecast from "./Forcast";
 
-export default function Weather(props) { 
-    return (
-    <Text>{props.zipCode}</Text>
-    );
-   }
-   export default function Weather(props) {
-    const [forecastInfo, setForecastInfo] = useState({
-    main: '-',
-    description: '-',
-    temp: 0
-    }) 
-    return (
-        <View>
- <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
- <Text>Zip Code</Text>
- <Text>{props.zipCode}</Text>
- <Forecast {...forecastInfo} />
- </ImageBackground>
- </View>
- );
+export default function Weather(props) {
+  const [forecastInfo, setForecastInfo] = useState({
+    main: "-",
+    description: "-",
+    temp: 0,
+  })
+  return (
+
+    <ImageBackground source={require("../bg.jpg")} style={styles.backdrop}>
+      <Text>Zip Code</Text>
+      <Text>{props.zipCode}</Text>
+      <Forecast {...forecastInfo} />
+      </ImageBackground>
+
+  )
 }
 const styles = StyleSheet.create({
- backdrop: {
- alignItems: 'center',
- width: '100%',
- height: '100%'
- },
-}); 
+    backdrop: {
+      alignItems: "center",
+      width: "100%",
+      height: "100%",
+    }
+  }) 
